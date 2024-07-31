ac1402fd216dd2592b7a6bf26e517224b645ecdd
tantrinh29 <dinhtrinh5678@gmail.com>
2024-07-31 10:10:43 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: '0.0.0.0', // Lắng nghe trên tất cả các giao diện mạng
+    port: process.env.PORT || 5173, // Sử dụng cổng từ biến môi trường hoặc cổng 5173
+  }
+});
